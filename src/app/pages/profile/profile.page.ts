1dbe878951d819773a969f7a3deeefa5467bc0a7
Néstor Chumania <nestor.chumania@epn.edu.ec>
2023-12-31 18:18:42 -0500

    Falta mandar a la base de datos

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -27,18 +27,21 @@ export class ProfilePage implements OnInit {
   recordingCountdownValue = 5;
   recording = false;
   successMessage: string | null = null;
+  latitude: number | null = null;
+  longitude: number | null = null;
+  locationLink!: string;
+  showLocationButton = true;
+  showLocationLink = false;
 
+  //
 
-//
+  recordingAudio = false;
+  audioRecorded = false;
+  audioUrl!: string;
+  audioStream!: MediaStream;
+  recordingAudioCountdownValue = 3; // Tiempo para iniciar la grabación
 
-
-recordingAudio = false;
-audioRecorded = false;
-audioUrl!: string;
-audioStream!: MediaStream;
-recordingAudioCountdownValue = 3; // Tiempo para iniciar la grabación
-
-//
+  //
 
   constructor(
     private authService: AuthService,
@@ -51,50 +54,80 @@ recordingAudioCountdownValue = 3; // Tiempo para iniciar la grabación
   ngOnInit() {
     this.authService.user$.subscribe((user) => (this.user = user));
   }
+  async getLocation() {
+    try {
+      const position = await this.getCurrentPosition();
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+
+      if (this.latitude !== null && this.longitude !== null) {
+        this.locationLink = `https://www.google.com/maps/search/?api=1&query=${this.latitude},${this.longitude}`;
+        console.log('Ubicación:', this.locationLink);
+        this.showLocationButton = false; // Oculta el botón después de obtener la ubicación
+        this.showLocationLink = true; // Muestra el enlace de la ubicación
+      }
+    } catch (error) {
+      console.error('Error al obtener la ubicación:', error);
+      // Maneja el error aquí, muestra un mensaje al usuario o realiza alguna acción.
+    }
+    this.changeDetector.detectChanges();
+
+  }
+  // ... (código existente) ...
+
+  openLocation() {
+    window.open(this.locationLink, '_blank'); // Abre el enlace en una nueva pestaña
+  }
+
+
+  getCurrentPosition(): Promise<GeolocationPosition> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => resolve(position),
+        (error) => reject(error)
+      );
+    });
+  }
 
   async startAudioRecordingCountdown() {
     this.recordingAudio = true; // Iniciar la grabación de audio
     this.audioRecorded = false; // Asegurarse de que el audio no se haya grabado aún
-  
+
     this.successMessage = `El audio está siendo grabado...`;
     this.changeDetector.detectChanges();
-  
+
     setTimeout(async () => {
       await this.startAudioRecording();
+      this.showLocationButton = true;
     }, this.recordingAudioCountdownValue * 1000);
   }
-  
+
   async startAudioRecording() {
     const audioData = await this.emergencyService.recordAudio();
     this.audioUrl = audioData.url;
     this.audioRecorded = true;
     this.audioStream = audioData.stream;
-  
+
     this.successMessage = 'Grabación de audio completada';
     this.changeDetector.detectChanges();
-      // Detener el micrófono una vez se ha grabado el audio
-  const audioTracks = this.audioStream.getAudioTracks();
-  audioTracks.forEach((track) => {
-    track.stop();
-  });
+    // Detener el micrófono una vez se ha grabado el audio
+    const audioTracks = this.audioStream.getAudioTracks();
+    audioTracks.forEach((track) => {
+      track.stop();
+    });
   }
-  
-  
-  
 
   async toggleAudioRecording() {
     await this.startAudioRecordingCountdown();
   }
 
-
-
-
   async takePicture() {
     this.pictureUrl = await this.emergencyService.takePicture();
     console.log(this.pictureUrl);
     this.pictureTaken = true;
-    this.successMessage = 'Foto tomada con éxito, a continuación se grabará un video de 5 segundos';
-    this.changeDetector.detectChanges(); 
+    this.successMessage =
+      'Foto tomada con éxito, a continuación se grabará un video de 5 segundos';
+    this.changeDetector.detectChanges();
   }
 
   async recordVideo() {
@@ -113,11 +146,13 @@ recordingAudioCountdownValue = 3; // Tiempo para iniciar la grabación
   async startRecording() {
     if (this.pictureTaken) {
       this.countdownValue = 3;
-  
-      const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
       this.videoStream = mediaStream;
       this.videoElement.nativeElement.srcObject = this.videoStream;
-  
+
       const countdownInterval = setInterval(() => {
         this.countdownValue--;
         if (this.countdownValue === 0) {
@@ -127,31 +162,31 @@ recordingAudioCountdownValue = 3; // Tiempo para iniciar la grabación
       }, 1000);
     }
   }
-  
+
   async startRecordingAfterCountdown() {
     this.recording = true;
     this.successMessage = null;
-  
+
     const mediaRecorder = new MediaRecorder(this.videoStream);
     const chunks: any[] = [];
-    let recordingSeconds = 5; 
-  
+    let recordingSeconds = 5;
+
     mediaRecorder.start();
-  
+
     const countdownInterval = setInterval(() => {
-      recordingSeconds--; 
-      this.recordingCountdownValue = recordingSeconds; 
-  
+      recordingSeconds--;
+      this.recordingCountdownValue = recordingSeconds;
+
       if (recordingSeconds === 0) {
         clearInterval(countdownInterval);
         mediaRecorder.stop();
       }
     }, 1000);
-  
+
     mediaRecorder.ondataavailable = (event) => {
       chunks.push(event.data);
     };
-  
+
     mediaRecorder.onstop = (event) => {
       const blob = new Blob(chunks, { type: 'video/mp4' });
       this.videoUrl = URL.createObjectURL(blob);
@@ -161,24 +196,22 @@ recordingAudioCountdownValue = 3; // Tiempo para iniciar la grabación
       this.videoStream.getTracks().forEach((track) => track.stop());
       this.changeDetector.detectChanges();
     };
-  
+
     setTimeout(() => {
       mediaRecorder.stop();
-    }, 5005); 
+    }, 5005);
   }
-  
-  
-  
+
   async emergency() {
     await this.takePicture();
   }
 
   toggleEdit() {
     if (this.editing) {
-      this.user = this.editUser; 
+      this.user = this.editUser;
       this.updateProfile();
     } else {
-      this.editUser = { ...this.user }; 
+      this.editUser = { ...this.user };
     }
     this.editing = !this.editing;
   }
@@ -192,7 +225,7 @@ recordingAudioCountdownValue = 3; // Tiempo para iniciar la grabación
         event,
         this.user?.uid || ''
       );
-      this.editUser.photoURL = url; 
+      this.editUser.photoURL = url;
     }
   }
 
